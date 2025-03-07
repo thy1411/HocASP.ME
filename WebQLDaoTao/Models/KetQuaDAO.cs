719f9000484aedd032fea50ca728414627b3aeca
ADMIN <ADMIN@PC243>
2025-03-07 10:37:44 +0700

    Add project files.

---
+++ WebQLDaoTao/Models/KetQuaDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebQLDaoTao.Models
+{
+    public class KetQuaDAO
+    {
+        public List<KetQua> getAll()
+        {
+            List<KetQua> ds = new List<KetQua>();
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringName"].ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from ketqua", conn);
+            SqlDataReader rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                KetQua kq = new KetQua()
+                {
+                    Id = int.Parse(rd["id"].ToString()),
+                    MaSV = rd["MaSV"].ToString(),
+                    MaMH = rd["MaMH"].ToString(),
+                };
+                if (!string.IsNullOrEmpty(rd["diem"].ToString()))
+                {
+                    kq.Diem = float.Parse(rd["diem"].ToString());
+                }
+                ds.Add(kq);
+            }
+            return ds;
+        }
+    }
+}
---
+++ WebQLDaoTao/NhapDiem.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebQLDaoTao
+{
+    public partial class NhapDiem : System.Web.UI.Page
+    {
+        //KetQuaDAO
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
